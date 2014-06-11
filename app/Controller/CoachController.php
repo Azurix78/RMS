96942c889a347e9a99e8129b200daaa7b9746c9c
Azurix78 <ni.rubio78@gmail.com>
2014-06-11 23:13:00 +0200

    rename coach

--- app/Controller/CoachController.php
+++ app/Controller/CoachController.php
@@ -1,17 +1,15 @@
 <?php
-
-class CoatchController extends AppController {
+class CoachController extends AppController {
 
 	public function admin_index() {
 
 	}
 
 	public function admin_edit($id = 1) {
-		$data = $this->Coatch->find('first', array('conditions' => array('coatch_id' => $id)));
+		$data = $this->Coach->find('first', array('conditions' => array('coach_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
 
 		}
 	}
 }
-
 ?>
---
+++ app/Model/Coach.php
@@ -0,0 +1,8 @@
+<?php
+
+class Coach extends AppModel {
+	public $primaryKey = 'coach_id';
+	public $useTable = 'coach';
+}
+
+?>
