7a454c21ca50b3b771379cddbab750222c4b5065
Raestas <remy.mulet@epitech.eu>
2014-06-12 00:14:04 +0200

    De la mort

--- app/Controller/CoachController.php
+++ app/Controller/CoachController.php
@@ -1,11 +1,20 @@
 <?php
 class CoachController extends AppController {
 
-	public function admin_edit($id = 1) {
+	public function admin_index($id = 1) {
 		$data = $this->Coach->find('first', array('conditions' => array('coach_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
-			
+			$d = $this->request->data;
+			$this->Coach->id = $id;
+			if ($this->Coach->save($d, true, array('coach_content'))) {
+				$this->Session->setFlash("La page coaching à bien été édité !", 'notif');
+				$this->redirect($this->referer());
+			} else {
+				$this->Session->setFlash("Un problème est survenu !", 'notif', array('type' => 'error'));
+				$this->redirect($this->referer());
+			}
 		}
+		$this->request->data = $data;
 	}
 }
 ?>
