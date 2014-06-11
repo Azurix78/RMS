3897e84faaad4f88d4b5cdeee4c5d30113dceaf9
Raestas <remy.mulet@epitech.eu>
2014-06-12 00:22:17 +0200

    finish admin

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -5,17 +5,20 @@ class AdminController extends AppController {
 	public $uses = array('Param');
 
 	public function admin_index() {
-
+		
 	}
 
-	public function admin_params() {
-		$data = $this->Param->find('first', array('conditions' => array('param_id' => 1)));
+	public function admin_params($id = 1) {
+		$data = $this->Param->find('first', array('conditions' => array('param_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
 			$d = $this->request->data;
-			if ($this->Param->save($d, true, array())) {
-				
+			$this->Param->id = $id;
+			if ($this->Param->save($d, true, array('param_mail_contact', 'param_info_contact', 'param_is_maintenance', 'param_message_maintenance', 'param_team_desc', 'param_aboit_ours'))) {
+				$this->Session->setFlash("Les parramètres ont bien été modifié !", 'notif');
+				$this->redirect($this->referer());
 			} else {
-				
+				$this->Session->setFlash("Un problème est survenu !", 'notif', array('type' => 'error'));
+				$this->redirect($this->referer());
 			}
 		}
 		$this->request->data = $data;
