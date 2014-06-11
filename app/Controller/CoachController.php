562b78b4494aab3e4bada880315a5fdc2a1caaaf
Raestas <remy.mulet@epitech.eu>
2014-06-11 23:58:48 +0200

    Revert "Revert "blalbla""
    This reverts commit 65d36921f693cad300e96e29e4a173a6c31e4bca.

--- app/Controller/CoachController.php
+++ app/Controller/CoachController.php
@@ -1,10 +1,6 @@
 <?php
 class CoachController extends AppController {
 
-	public function admin_index() {
-
-	}
-
 	public function admin_edit($id = 1) {
 		$data = $this->Coach->find('first', array('conditions' => array('coach_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
