d57a1cae3b625e75066bca952ea08763354baec9
Raestas <remy.mulet@epitech.eu>
2014-06-11 23:58:59 +0200

    Revert "blalbla"
    This reverts commit 71e73413d2391db0268ad66cc4235185f4feaf99.

--- app/Controller/CoachController.php
+++ app/Controller/CoachController.php
@@ -1,6 +1,10 @@
 <?php
 class CoachController extends AppController {
 
+	public function admin_index() {
+
+	}
+
 	public function admin_edit($id = 1) {
 		$data = $this->Coach->find('first', array('conditions' => array('coach_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
