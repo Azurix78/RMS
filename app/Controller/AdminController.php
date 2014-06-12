b758f443863150a2cd26b84cb7f3f63e01732f9a
Azurix78 <ni.rubio78@gmail.com>
2014-06-12 15:22:11 +0200

    add view index

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -4,17 +4,13 @@ class AdminController extends AppController {
 
 	public $uses = array('Param');
 
-	public function admin_index() {
-		
-	}
-
-	public function admin_params($id = 1) {
+	public function admin_index($id = 1) {
 		$data = $this->Param->find('first', array('conditions' => array('param_id' => $id)));
 		if ($this->request->is('post') || $this->request->is('put')) {
 			$d = $this->request->data;
 			$this->Param->id = $id;
-			if ($this->Param->save($d, true, array('param_mail_contact', 'param_info_contact', 'param_is_maintenance', 'param_message_maintenance', 'param_team_desc', 'param_aboit_ours'))) {
-				$this->Session->setFlash("Les parramètres ont bien été modifié !", 'notif');
+			if ($this->Param->save($d, true, array('param_about_ours'))) {
+				$this->Session->setFlash("Les parramètres ont bien été modifiés !", 'notif');
 				$this->redirect($this->referer());
 			} else {
 				$this->Session->setFlash("Un problème est survenu !", 'notif', array('type' => 'error'));
