a87bf7666af5841450deee0c8d75aabdbbb48fc7
happy1337 <happy-666@hotmail.fr>
2014-06-26 17:04:11 +0200

    menu responsive

--- app/Controller/SlidersController.php
+++ app/Controller/SlidersController.php
@@ -13,7 +13,7 @@ class SlidersController extends AppController {
 			$d['Slider']['slider_img'] = $this->upload_file($d['Slider']['slider_img'], 'sliders');
 			if ($d['Slider']['slider_img'] != false) {
 				if ($this->Slider->save($d, true, array('slider_id', 'slider_name', 'slider_desc', 'slider_img'))) {
-					$this->Session->setFlash("L'article à bien été ajouté !", 'notif');
+					$this->Session->setFlash("Le slide à bien été ajouté !", 'notif');
 					$this->redirect(array('controller' => 'sliders', 'action' => 'index', 'admin' => true));
 				} else {
 					$this->Session->setFlash("Un problème est survenu, réessayer !",'notif', array('type' => 'error'));
@@ -33,7 +33,7 @@ class SlidersController extends AppController {
 				if ($this->Slider->save($d, true, array('slider_name', 'slider_desc', 'slider_img'))) {
 					if ($d['Slider']['slider_img'] != $data['Slider']['slider_img'])
 						$this->delete_file($data['Slider']['slider_img'], 'sliders');
-					$this->Session->setFlash("L'article à bien été supprimé !", 'notif');
+					$this->Session->setFlash("Le slide a bien édité !", 'notif');
 					$this->redirect(array('controller' => 'sliders', 'action' => 'index', 'admin' => true));
 				} else {
 					$this->Session->setFlash("Un problème est survenu, réessayer !", 'notif', array('type' => 'error'));
@@ -49,7 +49,7 @@ class SlidersController extends AppController {
 		$data = $this->Slider->find('first', array('conditions' => array('slider_id' => $id)));
 		if ($this->delete_file($data['Slider']['slider_img'], 'sliders')) {
 			$this->Slider->delete($id);
-			$this->Session->setFlash("L'article à bien été supprimé !", 'notif');
+			$this->Session->setFlash("Le slide bien été supprimé !", 'notif');
 			$this->redirect($this->referer());
 		}
 	}
@@ -59,7 +59,7 @@ class SlidersController extends AppController {
 		$data = $this->Slider->find('first', array('conditions' => array('slider_id' => $id)));
 		$this->Slider->id = $data['Slider']['slider_id'];
 		($data['Slider']['slider_is_activated'] == 0) ? $this->Slider->saveField('slider_is_activated', 1) : $this->Slider->saveField('slider_is_activated', 0);
-		$this->Session->setFlash("l'article à bien été modifié !", 'notif');
+		$this->Session->setFlash("Activation / Desactivation effectué !", 'notif');
 		$this->redirect(array('controller' => 'sliders', 'action' => 'index', 'admin' => true));
 	}
 }
